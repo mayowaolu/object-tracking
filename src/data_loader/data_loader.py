e8ff2426a32c23ac122c9aa6dc38d3d0e001c7d4
John Olusetire <johnolusetire97@gmail.com>
2025-01-03 01:57:16 -0500

    Add data classes for bounding boxes and image annotations in data_loader.py

--- src/data_loader/data_loader.py
+++ src/data_loader/data_loader.py
@@ -1,13 +1,38 @@
 import os
 import xml.etree.ElementTree as ET
 from cv2 import imread
+from dataclasses import dataclass, field
+from typing import List
 
-# Current Path
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# Define data structure
+@dataclass
+class BoundingBox:
+    xmin: int
+    ymin: int
+    xmax: int
+    ymax: int
 
-# Project Root
-project_root = os.path.join(current_dir, "..", "..")
+@dataclass
+class ObjectAnnotation:
+    class_name: str
+    bbox: BoundingBox
 
-# Construct paths to other directories
-data_dir = os.path.join(project_root, "data")
-raw_data_dir = os.path.join(project_root, "raw")
+@dataclass
+class ImageData:
+    filename: str
+    filepath: str
+    width: int
+    height: int
+    objects: List[ObjectAnnotation] = field(default_factory=list)
+
+
+
+# # Current Path
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# # Project Root
+# project_root = os.path.join(current_dir, "..", "..")
+
+# # Construct paths to other directories
+# data_dir = os.path.join(project_root, "data")
+# raw_data_dir = os.path.join(project_root, "raw")
