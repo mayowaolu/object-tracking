c032c7a6aea2e2688a5091743a25344c149e41b5
John Olusetire <johnolusetire97@gmail.com>
2025-01-08 18:20:10 -0500

    Enhance data split creation with file validation and progress logging

--- src/data_loader/data_loader.py
+++ src/data_loader/data_loader.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass, field
 from typing import List, Tuple
 import numpy as np
 import logging
+from tqdm import tqdm
 
 # Define data structure
 @dataclass
@@ -144,16 +145,47 @@ def create_data_splits(dataset_root: str | Path) -> Tuple[List[ImageData], List[
         
         # Process Split Files
         train_paths = []
+        for train_file, year_path in zip(train_files, [paths['voc07'], paths['voc12']]):
+            image_ids = [id.strip() for id in train_file.read_text().splitlines()]
+            train_paths.extend([
+                (year_path['images'] / f"{id}.jpg", year_path['annotations'] / f"{id}.xml") 
+                for id in image_ids
+            ])
         
+        year_path = paths['voc07']
+        image_ids = [id.strip() for id in test_file.read_text().splitlines()]
+        test_paths = [
+            (year_path['images'] / f"{id.strip()}.jpg", year_path['annotations'] / f"{id.strip()}.xml") 
+            for id in image_ids
+        ]
 
+        # Image and Annotation file validation
+        missing_files = [
+            (img, annot) for img, annot in [*train_paths, *test_paths]
+            if not img.exists() or not annot.exists()
+        ]
+        if missing_files:
+            raise DatasetPathError(f"Missing {len(missing_files)} files, first few: {missing_files[:3]}")
 
+        logging.info()
         # Create data samples and append to list
         train_data = []
+        for image_path, annotation_path in tqdm(train_paths, desc="Creating train data", ncols=500):
+            data_sample = get_data_sample(image_path, annotation_path)
+            train_data.append(data_sample)
 
         test_data = []
+        for image_path, annotation_path in tqdm(test_paths, desc="Creating train data", ncols=500):
+            data_sample = get_data_sample(image_path, annotation_path)
+            test_data.append(data_sample)
 
+        logging.info(f"Succesfully created data splits with {len(train_data)} training and {len(test_data)} testing images")
+
+        return train_data, test_data
     
-    except:
-        pass
+    except (ValueError, TypeError) as e:
+        raise ValueError(f"Invalid dataset root path: {dataset_root}") from e
+    except (DatasetPathError) as e:
+        raise DatasetPathError(f"Error processing dataset: {str(e)}") from e
+       
     
-    return train_data, test_data
